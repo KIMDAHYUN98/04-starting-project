c09290eacd324d9f07eb37b7069a8950c26ae84e
dev_mong <74331917+KIMDAHYUN98@users.noreply.github.com>
2025-12-30 23:50:50 +0900

    🎉Section 9: 연습 프로젝트 - 152. 컴포넌트 간 교환을 위한 State(상태) 관리법

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,14 +1,14 @@
 import noProjectImage from '../assets/no-projects.png';
 import Button from './Button.jsx'
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({onStartAddProject}) {
     return (
         <div className="mt-24 text-center w-2/3">
             <img src={noProjectImage} alt="An empty task list" className="w-16 h-16 object-contain mx-auto"/>
             <h2 className="text-xl font-bold text-stone-500 mt-4">선택된 프로젝트가 없습니다.</h2>
             <p className="text-stone-400 mb-4">프로젝트를 선택하거나 새 프로젝트로 시작하세요.</p>
             <p className="mt-8">
-                <Button>새 프로젝트 만들기</Button>
+                <Button onClick={onStartAddProject}>새 프로젝트 만들기</Button>
             </p>
         </div>
     );
